1354d4eda27164fc51507539d6fdb776fd935385
thedreamJK7 <jkubaev7@gmail.com>
2021-11-09 11:40:06 +0500

    done

--- js/main.js
+++ js/main.js
@@ -1,22 +1,48 @@
 const apiKey = 'B_RJTYn7umfIdgUgbmlYwwm2PR7XiVyH6Ok0MGS3TqY';
-const count = 10;
+const count = 30;
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 const imgContainer = document.getElementById('img');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imageLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
+function imageLoader() {
+    imageLoaded++;
+    if(imageLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
 function photosCreate() {
+    totalImages = photosArray.length;
     photosArray.forEach(element => {
         const ahref = document.createElement('a');
         const img = document.createElement('img');
-        ahref.setAttribute('href', element.links.html);
-        ahref.setAttribute('target', '_blank');
+        createAttribute(ahref, {
+            href: element.links.html,
+            target: '_blank'
+        })
         
-        img.setAttribute('src', element.urls.regular);
-        img.setAttribute('alt', element.alt_description);
+        createAttribute(img, {
+            src: element.urls.regular,
+            alt: element.alt_description
+        })
+
+        img.addEventListener('load', imageLoader);
         
         ahref.appendChild(img);
         imgContainer.appendChild(ahref);
     });
+}
+
+function createAttribute(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
 }
 
 async function getPhotos() {
@@ -29,4 +55,11 @@ async function getPhotos() {
     }
 }
 
+window.addEventListener('scroll', ()=> {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        imageLoaded = 0;
+        getPhotos();
+    }
+})
 getPhotos();
